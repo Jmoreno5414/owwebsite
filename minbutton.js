1aad60b4a1b93ee8a517500bc90bb132e7e20252
Juan M <jjuandm2006@gmail.com>
2026-09-21 11:38:28 -0400

    working on popup

---
+++ minbutton.js
@@ -0,0 +1,86 @@
+const button1 = document.querySelector(".lm1");
+const button2 = document.querySelector(".lm2");
+const button3 = document.querySelector(".lm3");
+const button4 = document.querySelector(".lm4");
+const button5 = document.querySelector(".lm5");
+const button6 = document.querySelector(".lm6");
+
+
+
+const titlearray = ["KIDS", "STUDENTS", "NEXT GEN", "WORSHIP", "SMALL GROUPS", "I-TEAM"]
+const spanarray = ["Ages: 5-12", "Middle school - College", "Young adults", "", "", ""]
+
+const loopimages = [""]
+const popupframe = document.querySelector(".pop-up-block")
+const popuptitle = document.querySelector(".pop-up-title")
+const popupdesc = document.querySelector(".pop-up-desc")
+
+
+
+
+
+/* <div class="ow-text-block"> 
+                <h2 class="kids-title">KIDS <span>Ages: 5-12</span></h2> 
+                <h3 class="kids-description">asdar jwajajd ajje dnans tbrm bur wak skfk xoao lr was gs aeafd brtgrs</h3> 
+                <button type="button" class="button-reuse lm1"><h2 class="learn-more"> LEARN MORE →</h2> </button>
+            </div> 
+            
+        </div> 
+
+        <!-- Student Block -->
+        <div class="student-block"> 
+            <img src="../images/studentminis.jpg" class="student-logo"> 
+            <div class="student-text-block"> 
+                <h2 class="student-title">STUDENTS <span>Middle school - College</span></h2> 
+                <h3 class="student-description">asdar jwajajd ajje dnans tbrm bur wak skfk xoao lr was gs aeafd brtgrs</h3> 
+                <button type="button" class="button-reuse lm2"><h2 class="learn-more"> LEARN MORE →</h2> </button>
+            </div> 
+
+        </div> 
+
+        <!-- Young Adult Block -->
+        <div class="yadult-block"> 
+            <img src="../images/nextgenlogo.PNG" class="yadult-logo"> 
+            <div class="yadult-text-block"> 
+                <h2 class="yadult-title">NEXT GEN <span>Young adults</span></h2> 
+                <h3 class="yadult-description">asdar jwajajd ajje dnans tbrm bur wak skfk xoao lr was gs aeafd brtgrs</h3> 
+                <button type="button" class="button-reuse lm3"><h2 class="learn-more"> LEARN MORE →</h2> </button>
+            </div> 
+        </div> 
+
+    </div>
+
+<!-- Block 2 -->
+    <div class="bottom-block"> 
+        <div class="kids-block"> 
+            <img src="../images/worship.png" class="ow-kids-logo"> 
+            <div class="ow-text-block"> 
+                <h2 class="kids-title">WORSHIP <span></span></h2> 
+                <h3 class="kids-description">asdar jwajajd ajje dnans tbrm bur wak skfk xoao lr was gs aeafd brtgrs</h3> 
+                <button type="button" class="button-reuse lm4"><h2 class="learn-more"> LEARN MORE →</h2> </button>
+            </div> 
+        </div> 
+
+        <!-- Student Block -->
+        <div class="student-block"> 
+            <img src="../images/smallgroup.png" class="student-logo"> 
+            <div class="student-text-block"> 
+                <h2 class="student-title">SMALL GROUPS <span></span></h2> 
+                <h3 class="student-description">asdar jwajajd ajje dnans tbrm bur wak skfk xoao lr was gs aeafd brtgrs</h3> 
+                <button type="button" class="button-reuse lm5"><h2 class="learn-more"> LEARN MORE →</h2> </button>
+            </div> 
+        </div> 
+
+        <!-- Young Adult Block -->
+        <div class="yadult-block"> 
+            <img src="../images/placeholder.png" class="yadult-logo"> 
+            <div class="yadult-text-block"> 
+                <h2 class="yadult-title">I - TEAM<span></span></h2> 
+                <h3 class="yadult-description">asdar jwajajd ajje dnans tbrm bur wak skfk xoao lr was gs aeafd brtgrs</h3> 
+                <button type="button" class="button-reuse lm6"><h2 class="learn-more"> LEARN MORE →</h2> </button>
+            </div> 
+        </div> 
+    </div>
+
+
+ */
